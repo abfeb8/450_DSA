8be9b8884ac75581a9d720b2bfd785fbb738fc5b
Abhishek <abfeb8@gmail.com>
2020-11-15 10:20:32 +0530

    kadanes

--- Array/13_kadaneAlgo.cpp
+++ Array/13_kadaneAlgo.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void kadan(vector<int> arr, int n)
+void kadanes_MaxSubArraySum(vector<int> arr, int n)
 {
     int max_sum = 0, current_max = 0;
     int start = 0, end = 0, s = 0;
@@ -10,13 +10,37 @@ void kadan(vector<int> arr, int n)
         current_max = max(arr[i], current_max + arr[i]);
         max_sum = max(max_sum, current_max);
     }
-    cout << max_sum;
+    cout << max_sum << endl;
+}
+
+void printMaxSumSubArray(vector<int> arr, int n)
+{
+    int max_sum = 0, current_max = 0;
+    int start = 0, end = 0;
+    for (int i = 0; i < n; i++)
+    {
+        current_max = current_max + arr[i];
+        if (max_sum < current_max)
+        {
+            max_sum = current_max;
+            end = i;
+        }
+        if (current_max < 0)
+        {
+            current_max = 0;
+            start = i + 1;
+        }
+    }
+    for (int i = start - 1; i < end + 1; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 }
 
 int main()
 {
     vector<int> arr = {-2, 1, 3, -5, 2, 4, -3, 4, 1};
     int n = arr.size();
-    kadan(arr, n);
+    kadanes_MaxSubArraySum(arr, n);
+    printMaxSumSubArray(arr, n);
     return 0;
 }
